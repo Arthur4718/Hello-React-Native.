d7922c9c2043fa305ea5f3f6559751287b5f84a2
Arthur Gomes <arthur@megaleios.com>
2019-11-21 16:45:21 -0300

    Navigation view

---
+++ src/pages/main.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+import {View, Text} from "react-native"
+
+
+export default class Main extends Component{
+
+    static navigationOptions = {
+        title : "Main Page"
+    };
+
+    render(){
+        return(
+            <View>
+                <Text>Main Page Content </Text>
+            </View>
+
+        );
+    }
+}

4d29666005a3431dac050508f196649da9c74891
Arthur Gomes <arthur@megaleios.com>
2019-11-22 17:49:54 -0300

    creting requests using Axios

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,18 +1,40 @@
-import React, {Component} from "react"
+import React, {Component} from "react";
+import api from '../services/api';
 
-import {View, Text} from "react-native"
+import {View, Text} from "react-native";
 
-
+//
 export default class Main extends Component{
 
     static navigationOptions = {
         title : "Main Page"
     };
 
+
+    state = { 
+      docs : [],
+    };
+
+    componentDidMount() { 
+        this.loadProducts();
+    }
+
+    loadProducts = async() => { 
+        const response = await api.get("/products"); 
+
+        const{ docs } = response.data; 
+
+        this.setState({ docs });
+    };
+
     render(){
         return(
             <View>
-                <Text>Main Page Content </Text>
+                <Text>Main Page: </Text>
+                {this.state.docs.map(product =>{ 
+                    return <Text>{product.title}</Text>
+                })}
+                
             </View>
 
         );
